3b7866ec2ffde5881f7212fb96c502265aae40a2
pultzlucas <pultzlucas@gmail.com>
2023-07-31 15:25:35 -0300

    logging into accounts

--- gptconnect.py
+++ gptconnect.py
@@ -1,31 +1,50 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import undetected_chromedriver as uc
+import json
+import base64
+import threading
 
 class GPTConnect():
     def __init__(self):
-        options = uc.ChromeOptions()
-        # options.headless=True
-        # options.add_argument('--headless')
-        # chrome = uc.Chrome(options=options)
-        self.drive = uc.Chrome()
+        with open('accounts.json') as accounts:
+            self.accounts = json.loads(accounts.read())
     
-    def load(self):
-        self.drive.get("https://chat.openai.com/auth/login")
-        self.drive.implicitly_wait(20)
-        self.login()
-        self.remove_popups()
+    def load(self, scraper_path):
+        for account in self.accounts:
+            options = uc.ChromeOptions()
+            # options.add_experimental_option("detach", True)
+            self.drive = uc.Chrome(options=options)
+            self.drive.get("https://chat.openai.com/auth/login")
+            self.drive.implicitly_wait(20)
+            self.login(account['username'], account['password'], account['auth_method'])
+            self.remove_popups()
+            self.execute_scraper(scraper_path)
 
-    def login(self):
+    def login(self, username, password, auth_method):
         self.drive.find_element(
             By.XPATH, '//button[@class="btn relative btn-primary"]').click()
-        self.drive.find_element(
-            By.XPATH, '//input[@name="username"]').send_keys('leandro.augusto.alves@outlook.com.br')
-        self.drive.find_element(By.XPATH, '//button[@type="submit"]').click()
-        self.drive.find_element(
-            By.XPATH, '//input[@name="password"]').send_keys('Zv15936jp!')
-        self.drive.find_element(By.CSS_SELECTOR, '._button-login-password').click()
-        self.drive.implicitly_wait(20)
+        
+        if auth_method == 'gpt':
+            self.drive.find_element(
+                By.XPATH, '//input[@name="username"]').send_keys(username)
+            self.drive.find_element(By.XPATH, '//button[@type="submit"]').click()
+            self.drive.find_element(
+                By.XPATH, '//input[@name="password"]').send_keys(base64.b64decode(password).decode('utf-8'))
+            self.drive.find_element(By.CSS_SELECTOR, '._button-login-password').click()
+
+        if auth_method == 'google':
+            self.drive.find_element(
+                By.XPATH, '//button[@class="c9896a921 c83665ce2 c0062c0fe"]').click()
+            self.drive.find_element(
+                By.XPATH, '//input[@class="whsOnd zHQkBf"]').send_keys(username)
+            self.drive.find_element(
+                By.XPATH, '//div[@id="identifierNext"]').click()
+            self.drive.find_element(
+                By.XPATH, '//input[@autocomplete="current-password"]').send_keys(base64.b64decode(password).decode('utf-8'))
+            self.drive.find_element(
+                By.XPATH, '//div[@id="passwordNext"]').click()
+
 
     def remove_popups(self):
         for _ in range(3):
--- main.py
+++ main.py
@@ -2,5 +2,4 @@ from gptconnect import GPTConnect
 
 if __name__ == '__main__':
     gpt = GPTConnect()
-    gpt.load()
-    gpt.execute_scraper('./libs/scraping.js')
+    gpt.load('./libs/scraping.js')
